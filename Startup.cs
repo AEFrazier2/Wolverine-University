6460de26bc56f6b7b6af303f98caaf30ee7ac9c8
AEFrazier2 <aef2expitdet2020@gmail.com>
2021-01-10 08:05:25 -0500

    Wolverine University
    ASP.Net Framework MVC 5 EF 6 with Code First approach

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Midterm_Demo.Startup))]
+namespace Midterm_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
